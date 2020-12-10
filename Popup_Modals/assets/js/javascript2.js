728b57e645deae7689cde0f587bc826c9b7596ef
AntonelaMalii <malii.antonela@isa.utm.md>
2020-12-10 20:36:13 +0200

    Pop-up Modals

---
+++ Popup_Modals/assets/js/javascript2.js
@@ -0,0 +1,20 @@
+const showAlert_body2 = () => {
+  document.getElementById('alert_body2').classList.add('show')
+  const scrollY = document.documentElement.style.getPropertyValue('--scroll-y');
+  const body = document.body;
+  body.style.height = '100vh';
+  body.style.overflowY = 'hidden';
+};
+const closeAlert_body2 = () => {
+  const body = document.body;
+  const scrollY = body.style.top;
+  body.style.position = '';
+  body.style.top = '';
+  body.style.height = '';
+  body.style.overflowY = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
+  document.getElementById('alert_body2').classList.remove('show');
+}
+window.addEventListener('scroll', () => {
+  document.documentElement.style.setProperty('--scroll-y', `${window.scrollY}px`);
+});
